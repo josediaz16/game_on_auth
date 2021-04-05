be3af6020ca097bedf8842fc57e1c1a34b2ed3fa
josediaz16 <jldiaz1612@gmail.com>
2021-04-05 11:03:58 -0500

    define some basic associations

--- lib/game_on_auth/relations/accounts.rb
+++ lib/game_on_auth/relations/accounts.rb
@@ -1,7 +1,11 @@
 module GameOnAuth
   module Relations
     class Accounts < ROM::Relation[:sql]
-      schema(:accounts, infer: true)
+      schema(:accounts, infer: true) do
+        associations do
+          has_one :user
+        end
+      end
     end
   end
 end
--- lib/game_on_auth/relations/users.rb
+++ lib/game_on_auth/relations/users.rb
@@ -1,7 +1,15 @@
 module GameOnAuth
   module Relations
     class Users < ROM::Relation[:sql]
-      schema(:users, infer: true)
+      schema(:users, infer: true) do
+        associations do
+          belongs_to :account
+        end
+      end
+
+      def with_accounts
+        combine(:account)
+      end
     end
   end
 end
